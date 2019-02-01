3fa9c11eee966e9cb748d4d760aa26458ee024db
Taryn Li <tarynli@hotmail.com>
2019-02-01 16:08:39 -0500

    routes created, react app minor changes

--- server/server.js
+++ server/server.js
@@ -1,13 +1,35 @@
 const express = require('express'),
 app = express(),
+dotenv = require('dotenv'),
 cors = require('cors'),
 bodyParser = require('body-parser');
+
+// this line reads all key value pairs in .env folder
+dotenv.config(); 
 
 app.use(bodyParser.urlencoded({ extended: false}))
 app.use(bodyParser.json())
 app.use(cors());
 
 const PORT = process.env.PORT || 8080;
+const SECRET_KEY = process.env.SECRET_KEY;
+
+
+var config = {
+    apiKey: SECRET_KEY,
+    authDomain: "pinterest-clone-taryn.firebaseapp.com",
+    databaseURL: "https://pinterest-clone-taryn.firebaseio.com",
+    projectId: "pinterest-clone-taryn",
+    storageBucket: "pinterest-clone-taryn.appspot.com",
+    messagingSenderId: "228241382538"
+  };
+  firebase.initializeApp(config);
+
+
+
+
+
+
 
 
 
