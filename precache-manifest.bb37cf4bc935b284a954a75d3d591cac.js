07edc5b9b7157a31acfb77345e1df1ad2b27dde1
Taryn Li <tarynli@hotmail.com>
2019-02-07 10:35:00 -0500

    Updates

---
+++ precache-manifest.bb37cf4bc935b284a954a75d3d591cac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6003b4e4ba192b1b6519b1e762c3ba6f",
+    "url": "/pinterest-clone/static/media/pinterest.6003b4e4.svg"
+  },
+  {
+    "revision": "a79d8bfdfd66b1f3c0b0794890e119ce",
+    "url": "/pinterest-clone/static/media/github-icon.a79d8bfd.svg"
+  },
+  {
+    "revision": "76fe8af99f40441116e6",
+    "url": "/pinterest-clone/static/js/runtime~main.76fe8af9.js"
+  },
+  {
+    "revision": "173c7dbfb10807fd5c1a",
+    "url": "/pinterest-clone/static/js/main.173c7dbf.chunk.js"
+  },
+  {
+    "revision": "e78e6aaef8ac0169cba3",
+    "url": "/pinterest-clone/static/js/1.e78e6aae.chunk.js"
+  },
+  {
+    "revision": "173c7dbfb10807fd5c1a",
+    "url": "/pinterest-clone/static/css/main.cdd11af5.chunk.css"
+  },
+  {
+    "revision": "cd506c9ea11a8ddfb24d4af27ea5094c",
+    "url": "/pinterest-clone/index.html"
+  }
+];
